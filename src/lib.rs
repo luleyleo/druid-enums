27b02aed55fdb52f8a76b5ee844723333f02b147
Akasaka Ryuunosuke <vladkorotnev@gmail.com>
2021-03-18 08:10:31 +0900

    Revert fixes

--- src/lib.rs
+++ src/lib.rs
@@ -123,11 +123,11 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         let (data_pattern, data_values) = data_of(&variant, "");
         quote! {
             (_, #enum_name::#variant_name #data_pattern) => {
+                ctx.children_changed();
                 match &mut self.#builder_name {
                     Some(widget) => match widget.is_initialized() {
                         true => { 
                             widget.update(ctx, #data_values, env);
-                            ctx.request_paint();
                         },
                         false => (),
                     },
