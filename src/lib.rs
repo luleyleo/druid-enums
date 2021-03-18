11a4660c9b491e5d34857010c82c795f1a78c0a7
Akasaka Ryuunosuke <vladkorotnev@gmail.com>
2021-03-18 21:15:45 +0900

    Fix screen getting stuck after #9 (#10)
    Calling children_changed is overkill because the actual hierarchy didn't change, but as a side effect it called request_paint which fixes the bug — so call that directly instead.

--- src/lib.rs
+++ src/lib.rs
@@ -123,8 +123,9 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         let (data_pattern, data_values) = data_of(&variant, "");
         quote! {
             (_, #enum_name::#variant_name #data_pattern) => {
+                ctx.request_paint();
                 match &mut self.#builder_name {
-                    Some(widget) => match widget.is_initialized() {
+                     Some(widget) => match widget.is_initialized() {
                         true => widget.update(ctx, #data_values, env),
                         false => ctx.children_changed(),
                     },
